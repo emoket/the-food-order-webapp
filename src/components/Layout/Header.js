320771bac18f8fa3c4b570bdc857f7ee776463de
박용진 <insight.emoket@gmail.com>
2022-06-21 10:53:44 +0900

    Button Change to HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/chicken-dishes.webp';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>TheMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img alt='The Meal Background' src={mealsImage} />
