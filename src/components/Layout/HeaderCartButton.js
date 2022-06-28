0263c4e6b193a4c19366596ff9eb633376790645
박용진 <insight.emoket@gmail.com>
2022-06-28 16:37:08 +0900

    카트 버튼에 모달 팝업 클릭이벤트 추가

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,10 +4,8 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const cartButtonClickHandler = (event) => {};
-
   return (
-    <button className={classes.button} onClick={cartButtonClickHandler}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon className={classes.icon} />
       </span>
